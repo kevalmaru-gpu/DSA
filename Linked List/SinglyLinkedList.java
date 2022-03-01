817be6f01cee36cb30bfa25bf605002c54cd15bc
kevalCoder9106 <kevalcoder@gmail.com>
2022-03-01 18:26:25 +0530

    organizing

---
+++ Linked List/SinglyLinkedList.java
@@ -0,0 +1,69 @@
+public class SinglyLinkedList {
+    
+    private class Node{
+        public int data;
+        public Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node headNode = null, tailNode = null;
+    
+    public void AddNode(int data){
+        Node newNode = new Node(data);
+
+        if (headNode == null){
+            headNode = newNode;
+            tailNode = newNode;
+        }
+        else{
+            tailNode.next = newNode;
+            tailNode = newNode;
+        }
+    }
+
+    public void DeleteNode(){
+        Node tmpNode = headNode;
+
+        while(tmpNode.next != tailNode){
+            tmpNode = tmpNode.next;
+        }
+
+        tailNode = tmpNode;
+        tailNode.next = null;
+    }
+
+    public void Display(){
+        Node tmpNode = headNode;
+        
+        while(tmpNode != tailNode){
+            System.out.println(tmpNode.data);
+            tmpNode = tmpNode.next;
+        }
+        System.out.println(tmpNode.data);
+    }
+
+    public void ClearScreen(){
+        System.out.println("\033[H\033[2J");
+        return;
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        System.out.println("Phase 1");
+        linkedList.AddNode(10);
+        linkedList.AddNode(20);
+        linkedList.Display();
+        System.out.println("\nPhase 2");
+        linkedList.AddNode(200);
+        linkedList.AddNode(400);
+        linkedList.Display();
+        System.out.println("\nPhase 3");
+        linkedList.DeleteNode();
+        linkedList.Display();
+    }
+}
