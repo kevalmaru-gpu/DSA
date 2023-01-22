de4b0f12f3724fcc32a056c590145d67054b4735
kevalCoder9106 <kevalcoder@gmail.com>
2023-01-22 22:16:02 +0530

    Valid Parenthesis

---
+++ LeetCode/Easy/ValidParanthesis.java
@@ -0,0 +1,44 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<Character>();
+
+        // Traversing the Expression
+        for (int i = 0; i < s.length(); i++) {
+            char x = s.charAt(i);
+
+            if (x == '(' || x == '[' || x == '{') {
+                stack.push(x);
+                continue;
+            }
+
+            if (stack.isEmpty())
+                return false;
+            char check;
+            switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[')
+                        return false;
+                    break;
+
+                case '}':
+                    check = stack.pop();
+                    if (check == '(' || check == '[')
+                        return false;
+                    break;
+
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '{')
+                        return false;
+                    break;
+            }
+        }
+
+        // Check Empty Stack
+        return (stack.isEmpty());
+    }
+}
