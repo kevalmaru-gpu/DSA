3eaeca63ea476a086cb259bfb640ef5fe3b0cf73
kevalCoder9106 <kevalcoder@gmail.com>
2023-02-28 21:24:39 +0530

    2 easy problems done :)

---
+++ LeetCode/Easy/ExcelSheetColumnNumber.java
@@ -0,0 +1,15 @@
+class ExcelSheetColumnNumber {
+    public int titleToNumber(String columnTitle) {
+        int res = 0;
+
+        if (columnTitle.length() == 1)
+            return (int)columnTitle.charAt(0) - 64; 
+
+        for (int i=0; i<columnTitle.length(); i++){
+            res *= 26;
+            res += (int)columnTitle.charAt(i) - 64;
+        }
+
+        return res;
+    }
+}
--- LeetCode/Medium/AddTwo.java
+++ LeetCode/Medium/AddTwo.java
@@ -41,7 +41,7 @@ class Solution {
 
 public class AddTwo {
     public static void main(String[] args) {
-        Solution s = new Solution();
+        ExcelSheetColumnNumber s = new ExcelSheetColumnNumber();
         s.addTwoNumbers(new ListNode(2, new ListNode(4, new ListNode(3))), new ListNode(5, new ListNode(6, new ListNode(4))));
     }    
 }
